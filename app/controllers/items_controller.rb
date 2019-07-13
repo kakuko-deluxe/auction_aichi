1c93a204df00b2852bf349b73dba8ae7a1ab00c9
kakuko-deluxe <ky77ry@gmail.com>
2019-07-13 17:19:58 +0900

    更新ページ 完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,11 @@ class ItemsController < ApplicationController
   def edit
   @item = Item.find(params[:id])
   end
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
 
 
   private
