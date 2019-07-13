82105aa366993032ccc844188c3bec5bfd949f21
kakuko-deluxe <ky77ry@gmail.com>
2019-07-13 16:49:02 +0900

    商品登録フォーム 完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,22 @@
 class ItemsController < ApplicationController
   def show
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:id]) #find => classメソッド
   end
   def index
     @items = Item.all
+  end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params) 
+    @item.save
+    redirect_to @item
+  end
+
+  private
+
+  def item_params #strongパラメーター セキュリティ面 フォームで入力された内容を制限する？
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
   end
 end
